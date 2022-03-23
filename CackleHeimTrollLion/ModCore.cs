5bdbad086bbebcfb9a7d2854435a67559a34c365
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2022-03-23 11:32:19 -0600

    Checkin Mod

--- CackleHeimTrollLion/ModCore.cs
+++ CackleHeimTrollLion/ModCore.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using BepInEx;
+using CreatureManager;
 using HarmonyLib;
 
-namespace ModFrame
+namespace CackleHeimTrollLion
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class NewMod : BaseUnityPlugin
+    public class CackleHeimTrollMod : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
-        private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
+        internal const string ModName = "CackleHeimLionTroll";
+        internal const string ModVersion = "0.0.1";
+        private const string ModGUID = "come.cacklehieim.liontroll";
         private static Harmony harmony = null!;
         
         public void Awake()
@@ -17,6 +18,20 @@ namespace ModFrame
             Assembly assembly = Assembly.GetExecutingAssembly();
             harmony = new(ModGUID);
             harmony.PatchAll(assembly);
+
+            Creature LionTroll = new("liontroll", "Lion_Troll")
+            {
+                Biome = Heightmap.Biome.Meadows,
+                GroupSize = new Range(1,3),
+                CheckSpawnInterval = 600,
+                RequiredWeather = Weather.Rain | Weather.Fog | Weather.None| Weather.ClearSkies,
+                Maximum = 2
+            };
+            LionTroll.Localize().English("Lion Troll");
+            LionTroll.Drops["Wood"].Amount = new Range(1, 10);
+            LionTroll.Drops["Wood"].DropChance = 100f;
+            LionTroll.Drops["Wood"].MultiplyDropByLevel = false;
+            LionTroll.ConfigurationEnabled = true;
         }
     }
 }
